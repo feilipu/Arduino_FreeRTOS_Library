f44a556b20f77632517fb51eb52e7f6261762702
Phillip Stevens <phillip.stevens@gmail.com>
2016-09-21 12:26:17 +1000

    Update Blink_AnalogRead.ino
    Tested on the Arduino MICRO and LEONARDO. Requires the Serial port to start before the scheduler.
    Thanks to @SalvorinFex for testing these changes.

--- examples/Blink_AnalogRead/Blink_AnalogRead.ino
+++ examples/Blink_AnalogRead/Blink_AnalogRead.ino
@@ -6,6 +6,13 @@ void TaskAnalogRead( void *pvParameters );
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+  }
 
   // Now set up two tasks to run independently.
   xTaskCreate(
@@ -40,14 +47,38 @@ void TaskBlink(void *pvParameters)  // This is a task.
 {
   (void) pvParameters;
 
-  // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, LEONARDO, MEGA, and ZERO 
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care 
+  of use the correct LED pin whatever is the board used.
+  
+  The MICRO does not have a LED_BUILTIN available. For the MICRO board please substitute
+  the LED_BUILTIN definition with either LED_BUILTIN_RX or LED_BUILTIN_TX.
+  e.g. pinMode(LED_BUILTIN_RX, OUTPUT); etc.
+  
+  If you want to know what pin the on-board LED is connected to on your Arduino model, check
+  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
+  
+  This example code is in the public domain.
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+*/
+
+  // initialize digital LED_BUILTIN on pin 13 as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 
   for (;;) // A Task shall never return or exit.
   {
-    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
-    digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
     vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
   }
 }
@@ -55,9 +86,15 @@ void TaskBlink(void *pvParameters)  // This is a task.
 void TaskAnalogRead(void *pvParameters)  // This is a task.
 {
   (void) pvParameters;
+  
+/*
+  AnalogReadSerial
+  Reads an analog input on pin 0, prints the result to the serial monitor.
+  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
 
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  This example code is in the public domain.
+*/
 
   for (;;)
   {
@@ -65,6 +102,6 @@ void TaskAnalogRead(void *pvParameters)  // This is a task.
     int sensorValue = analogRead(A0);
     // print out the value you read:
     Serial.println(sensorValue);
-    vTaskDelay(1);  // one tick delay (30ms) in between reads for stability
+    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
   }
 }
