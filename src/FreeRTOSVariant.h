6e279e21ecac76b81dc70c1becdef1f49003856e
Phillip Stevens <phillip.stevens@gmail.com>
2018-01-04 19:32:33 +1100

    more fixes for uxTaskGetStackHighWaterMark()

--- src/FreeRTOSVariant.h
+++ src/FreeRTOSVariant.h
@@ -40,14 +40,14 @@ extern "C" {
 // System Tick - Scheduler timer
 // Use the Watchdog timer, and choose the rate at which scheduler interrupts will occur.
 
-#define portUSE_WDTO			WDTO_15MS	// portUSE_WDTO to use the Watchdog Timer for xTaskIncrementTick
+#define portUSE_WDTO		WDTO_15MS	// portUSE_WDTO to use the Watchdog Timer for xTaskIncrementTick
 
 /* Watchdog period options: 	WDTO_15MS
-								WDTO_30MS
-								WDTO_60MS
-								WDTO_120MS
-								WDTO_250MS
-								WDTO_500MS
+				WDTO_30MS
+				WDTO_60MS
+				WDTO_120MS
+				WDTO_250MS
+				WDTO_500MS
 */
 //	xxx Watchdog Timer is 128kHz nominal, but 120 kHz at 5V DC and 25 degrees is actually more accurate, from data sheet.
 #define configTICK_RATE_HZ		( (TickType_t)( (uint32_t)128000 >> (portUSE_WDTO + 11) ) )  // 2^11 = 2048 WDT scaler for 128kHz Timer
--- src/tasks.c
+++ src/tasks.c
@@ -856,7 +856,7 @@ UBaseType_t x;
 	by the port. */
 	#if( portSTACK_GROWTH < 0 )
 	{
-		pxTopOfStack = pxNewTCB->pxStack + ( ulStackDepth - ( uint32_t ) 1 );
+		pxTopOfStack = pxNewTCB->pxStack + ( ulStackDepth - ( configSTACK_DEPTH_TYPE ) 1 );
 		pxTopOfStack = ( StackType_t * ) ( ( ( portPOINTER_SIZE_TYPE ) pxTopOfStack ) & ( ~( ( portPOINTER_SIZE_TYPE ) portBYTE_ALIGNMENT_MASK ) ) ); /*lint !e923 MISRA exception.  Avoiding casts between pointers and integers is not practical.  Size differences accounted for using portPOINTER_SIZE_TYPE type. */
 
 		/* Check the alignment of the calculated top of stack is correct. */
@@ -879,7 +879,7 @@ UBaseType_t x;
 
 		/* The other extreme of the stack space is required if stack checking is
 		performed. */
-		pxNewTCB->pxEndOfStack = pxNewTCB->pxStack + ( ulStackDepth - ( uint32_t ) 1 );
+		pxNewTCB->pxEndOfStack = pxNewTCB->pxStack + ( ulStackDepth - ( configSTACK_DEPTH_TYPE ) 1 );
 	}
 	#endif /* portSTACK_GROWTH */
 
