f51e47fd811006443cf7d6f79aa4a74f3902cc8d
Phillip Stevens <phillip.stevens@gmail.com>
2023-05-03 14:27:03 +1000

    update 10.5.1+ - minor updates

--- src/atomic.h
+++ src/atomic.h
@@ -38,8 +38,8 @@
 #ifndef ATOMIC_H
 #define ATOMIC_H
 
-#ifndef INC_FREERTOS_H
-    #error "include FreeRTOS.h must appear in source files before include atomic.h"
+#ifndef INC_ARDUINO_FREERTOS_H
+    #error "Arduino_FreeRTOS.h must be included before atomic.h"
 #endif
 
 /* Standard includes. */
